aa56fb660e915d748ae80f8149cff4369c2f0c02
Mohd Shukri Hasan <mohdshukri@seekasia.com>
2019-01-19 20:29:51 +0800

    Removed unnecessary imports and functions.

--- explain/attention.py
+++ explain/attention.py
@@ -1,15 +1,9 @@
 import tensorflow as tf
-import tokenization
-from explain.model import load_bert_model
-import tempfile
-import os
 import numpy as np
 from xml.etree.ElementTree import Element, SubElement, tostring
 
 try:
-    from run_classifier import CfeProcessor, \
-        file_based_convert_examples_to_features, \
-        file_based_input_fn_builder
+    from run_classifier import file_based_convert_examples_to_features, file_based_input_fn_builder
 except tf.flags.DuplicateFlagError:
     pass
 
@@ -115,53 +109,3 @@ def viz_attention(tokens, token_weights, target_label, pred_label, pred_probs, r
         child.tail = ' '
 
     return tostring(top)
-
-
-def main():
-    vocab_file = './model/uncased_L-12_H-768_A-12/vocab.txt'
-    do_lower_case = True
-    data_dir = './data/CFE'
-    output_dir = tempfile.gettempdir() if data_dir is None else data_dir
-    predict_file = os.path.join(output_dir, 'predict.tf_record')
-    max_seq_length = 128
-
-    input_processor = CfeProcessor()
-
-    label_list = input_processor.get_labels()
-    estimator = load_bert_model(output_dir=output_dir,
-                                num_labels=len(label_list),
-                                attn_processor_fn=average_last_layer_by_head)
-
-    tokenizer = tokenization.FullTokenizer(vocab_file=vocab_file,
-                                           do_lower_case=do_lower_case)
-
-    predict_examples = input_processor.get_test_examples(data_dir)
-    file_based_convert_examples_to_features(predict_examples,
-                                            label_list,
-                                            max_seq_length,
-                                            tokenizer,
-                                            predict_file)
-    predict_input_fn = file_based_input_fn_builder(
-        input_file=predict_file,
-        seq_length=max_seq_length,
-        is_training=False,
-        drop_remainder=False
-    )
-
-    predictions = estimator.predict(input_fn=predict_input_fn)
-    vizs = []
-    for predict_example, prediction in zip(predict_examples, predictions):
-        input_tokens = _parse_input_text(predict_example.text_a, tokenizer, max_seq_length)
-        input_attention = prediction['attention']
-        viz = viz_attention(input_tokens,
-                            input_attention,
-                            predict_example.label,
-                            prediction['pred_class'],
-                            prediction['probabilities'],
-                            predict_example.guid)
-
-        vizs.append(viz)
-
-
-if __name__ == "__main__":
-    main()
